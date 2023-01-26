14a08e6216e90b8af799fe8f9a23295aa3603e63
Dmytro Lunhu <dlunhu@gmail.com>
2023-01-26 14:17:30 -0500

    First commit

---
+++ vwap-test-master/src/main/java/com/hsoft/practice/VwapTrigger.java
@@ -0,0 +1,32 @@
+package com.hsoft.practice;
+
+
+import com.hsoft.api.MarketDataListener;
+import com.hsoft.api.PricingDataListener;
+import com.hsoft.api.VwapTriggerListener;
+
+/**
+ * Entry point for the candidate to resolve the exercise
+ */
+public class VwapTrigger implements PricingDataListener, MarketDataListener {
+
+    private final VwapTriggerListener vwapTriggerListener;
+
+    public VwapTrigger(VwapTriggerListener vwapTriggerListener) {
+        this.vwapTriggerListener = vwapTriggerListener;
+    }
+
+    @Override
+    public void transactionOccurred(String productId, long quantity, double price) {
+        // This method will be called when a new transaction is received
+        // You can then perform your check
+        // And, if matching the requirement, notify the event via 'this.vwapTriggerListener.vwapTriggered(xxx);'
+    }
+
+    @Override
+    public void fairValueChanged(String productId, double fairValue) {
+        // This method will be called when a new fair value is received
+        // You can then perform your check
+        // And, if matching the requirement, notify the event via 'this.vwapTriggerListener.vwapTriggered(xxx);'
+    }
+}
