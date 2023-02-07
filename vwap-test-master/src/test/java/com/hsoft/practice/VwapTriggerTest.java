5c8f04a5231d9b21e0fd1e1d24397694aea08878
Dmytro Lunhu <dlunhu@gmail.com>
2023-02-07 12:42:25 -0500

    Add test fonctionality

--- vwap-test-master/src/test/java/com/hsoft/practice/VwapTriggerTest.java
+++ vwap-test-master/src/test/java/com/hsoft/practice/VwapTriggerTest.java
@@ -2,17 +2,19 @@ package com.hsoft.practice;
 
 import org.junit.jupiter.api.Test;
 
+import static com.hsoft.practice.VwapTrigger.vwapTriggerListener;
+
 /**
  * One can add own unit tests here and/or in another class
  */
 public class VwapTriggerTest {
 
     @Test
-    void compare() {
-        VwapTrigger vwapTrigger = new VwapTrigger();
-        {
+    VwapTrigger compare() throws Exception{
+        VwapTrigger vwapTrigger = vwapTriggerListener {
             vwapTrigger.transactionOccurred(String.valueOf(11), 11, 11);
             vwapTrigger.fairValueChanged(String.valueOf(22), 22);
         }
+        return vwapTrigger;
     }
 }
